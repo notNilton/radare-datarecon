0da24861c8afed48efd0a943bcb703bfc0454541
notNilton <nilton.naab@gmail.com>
2024-07-30 16:44:55 -0400

    "Removed brackets from progress string in ReconciliacaoModal"

--- src/components/Canva/utils/ReconciliacaoModal.tsx
+++ src/components/Canva/utils/ReconciliacaoModal.tsx
@@ -5,11 +5,14 @@ import "./ReconciliacaoModal.css";
 const ProgressModal = ({ isVisible, progress, onClose }) => {
   if (!isVisible) return null;
 
+  // Remove colchetes da string de progresso
+  const formattedProgress = progress.replace(/[\[\]]/g, "");
+
   return (
     <div className="progress-modal-overlay">
       <div className="progress-modal">
         <h2>Progresso da Reconciliação</h2>
-        <pre>{progress}</pre>
+        <pre>{formattedProgress}</pre>
         <button onClick={onClose} className="close-button">
           Fechar
         </button>
