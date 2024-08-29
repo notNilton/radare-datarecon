5913e8c4f6b271d61fda1cc3ce0f5b75fbeeb3c8
Nilton Aguiar dos Santos <nilton.naab@gmail.com>
2024-08-29 14:15:44 -0400

    feat: improve AboutModal

--- src/components/About/AboutModal.tsx
+++ src/components/About/AboutModal.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { Dialog } from "primereact/dialog";
-import { Button } from "primereact/button";
+import * as Dialog from "@radix-ui/react-dialog";
 import { AiOutlineClose } from "react-icons/ai";
+import { FaInfoCircle } from "react-icons/fa";
 import "./AboutModal.scss";
 
 interface AboutModalProps {
@@ -13,43 +13,37 @@ const AboutModal: React.FC<AboutModalProps> = ({
   showAbout,
   toggleAboutPopup,
 }) => {
-  const footer = (
-    <Button
-      label="Fechar"
-      icon={<AiOutlineClose />}
-      onClick={toggleAboutPopup}
-      className="p-button-outlined"
-    />
-  );
-
   return (
-    <Dialog
-      header="Sobre"
-      visible={showAbout}
-      style={{ width: "50vw" }}
-      footer={footer}
-      onHide={toggleAboutPopup}
-      className="about-dialog"
-      aria-labelledby="about-dialog-header"
-      aria-describedby="about-dialog-content"
-    >
-      <div id="about-dialog-content" className="about-content">
-        <p>
-          A ferramenta de Dashboard de Reconciliation de Dados foi proposta como
-          o projeto final para o curso de Engenharia de Computação na
-          Universidade Federal de Mato Grosso, Campus Várzea Grande. Este
-          projeto tem como objetivo facilitar a análise e visualização de dados,
-          fornecendo uma interface amigável e intuitiva para reconciliar dados,
-          de preferência Industriais.
-        </p>
-        <p>
-          Para entrar em contato com o desenvolvedor responsável pela
-          ferramenta, envie um e-mail para:{" "}
-          <a href="mailto:nilton.naab@gmail.com">nilton.naab@gmail.com</a>.
-          Feedbacks, sugestões e colaborações são sempre bem-vindos!
-        </p>
-      </div>
-    </Dialog>
+    <Dialog.Root open={showAbout} onOpenChange={toggleAboutPopup}>
+      <Dialog.Portal>
+        <Dialog.Overlay className="dialog-overlay" />
+        <Dialog.Content className="dialog-content">
+          <div className="dialog-header">
+            <FaInfoCircle className="header-icon" />
+            <Dialog.Title className="dialog-title">Sobre</Dialog.Title>
+            <Dialog.Close className="close-button">
+              <AiOutlineClose />
+            </Dialog.Close>
+          </div>
+          <div className="about-content">
+            <p>
+              A ferramenta de Dashboard de Reconciliation de Dados foi proposta
+              como o projeto final para o curso de Engenharia de Computação na
+              Universidade Federal de Mato Grosso, Campus Várzea Grande. Este
+              projeto tem como objetivo facilitar a análise e visualização de
+              dados, fornecendo uma interface amigável e intuitiva para
+              reconciliar dados, de preferência Industriais.
+            </p>
+            <p>
+              Para entrar em contato com o desenvolvedor responsável pela
+              ferramenta, envie um e-mail para:{" "}
+              <a href="mailto:nilton.naab@gmail.com">nilton.naab@gmail.com</a>.
+              Feedbacks, sugestões e colaborações são sempre bem-vindos!
+            </p>
+          </div>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
   );
 };
 
