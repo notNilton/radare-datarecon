ffd3fe43078d04dd01c2d8715331485dd21383cf
Nilton Santos <nilton.naab@gmail.com>
2024-08-30 17:12:02 -0400

    feat: adjust Graph onto Component, Hook and Api

--- src/components/Graph/GraphComponent.tsx
+++ src/components/Graph/GraphComponent.tsx
@@ -1,58 +1,19 @@
-import React, { useState, useEffect } from "react";
-import { Chart } from "primereact/chart";
-import "./GraphComponent.scss";
+// src/components/GraphComponent.tsx
+
+import React from 'react';
+import { Chart } from 'primereact/chart';
+import useGraphData from '../../hooks/GraphHook';
+import './GraphComponent.scss';
 
 const GraphComponent: React.FC = () => {
-  const [lineChartData, setLineChartData] = useState<any>(null);
-
-  // Função para fazer a requisição GET e processar os dados do backend
-  const fetchData = async () => {
-    try {
-      const response = await fetch('http://localhost:5000/reconciled-data');
-      const data = await response.json();
-      console.log('Dados recebidos do backend:', data);
-
-      if (data.length > 0) {
-        // Assume que todas as iterações têm os mesmos labels (nomes de tags)
-        const labels = data[0][0]; // Extrai os nomes das tags
-
-        // Cria um dataset para cada tag, agrupando os dados das correções
-        const datasets = labels.map((label: string, index: number) => ({
-          label: label,
-          data: data.map((iteration: any) => parseFloat(iteration[1][index])), // Extrai as correções para cada iteração
-          fill: false,
-          borderColor: `hsl(${index * 72}, 70%, 50%)`,
-        }));
-
-        const chartData = {
-          labels: data.map((_: any, index: number) => `Iteração ${index + 1}`), // Iterações no eixo X
-          datasets: datasets, // Dados das correções no eixo Y
-        };
-
-        setLineChartData(chartData);
-      } else {
-        setLineChartData(null);
-      }
-    } catch (error) {
-      console.error('Erro ao carregar dados do backend:', error);
-    }
-  };
-
-  useEffect(() => {
-    fetchData(); // Chama a função para buscar dados do backend
-
-    return () => {
-      // Remove qualquer evento de listener desnecessário
-      window.removeEventListener("storage", fetchData);
-    };
-  }, []);
+  const { lineChartData, error } = useGraphData();
 
   const lineChartOptions = {
     responsive: true,
     maintainAspectRatio: false,
     plugins: {
       legend: {
-        display: false, // Exibe a legenda com os nomes das tags
+        display: false,
       },
     },
   };
@@ -61,6 +22,7 @@ const GraphComponent: React.FC = () => {
     <div className="graph-component">
       <div className="graph-bar-title">Análise Resumida</div>
       <div className="graph-bar-content">
+        {error && <div>{error}</div>}
         {lineChartData ? (
           <Chart type="line" data={lineChartData} options={lineChartOptions} />
         ) : (
