793695353741276e7a78f90378426c3a213395a4
Nilton Santos <nilton.naab@gmail.com>
2025-02-11 12:06:52 -0400

    feat: implement graceful shutdown for HTTP server
    This commit implements graceful shutdown for the application's HTTP server to improve robustness and reliability in production, especially for containerized environments.
    
    Changes:
    
    - Uses `http.Server` struct for server configuration instead of direct `http.ListenAndServe`.
    - Adds signal handling for `SIGINT` and `SIGTERM` to detect shutdown requests.
    - Runs `server.ListenAndServe()` in a goroutine to allow main process to wait for shutdown signals.
    - Implements graceful shutdown using `server.Shutdown(ctx)` upon receiving an interrupt signal.
    - Configures a 30-second shutdown timeout.
    
    This enhancement ensures controlled server shutdown, prevents abrupt request interruptions, and improves compatibility with container orchestration.

--- backend/main.go
+++ backend/main.go
@@ -2,11 +2,15 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"radare/backend-new/handlers"
 	"radare/backend-new/middleware"
+	"syscall"
+	"time"
 )
 
 func main() {
@@ -20,9 +24,40 @@ func main() {
 		port = "8080"
 	}
 
-	// Inicia o servidor na porta definida
-	log.Println("Servidor iniciado na porta " + port + "...")
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
-		log.Fatalf("Erro ao iniciar o servidor: %v", err)
+	// Cria um servidor HTTP
+	server := &http.Server{
+		Addr:    ":" + port,
+		Handler: http.DefaultServeMux, // Usando o manipulador padrão para as rotas definidas com http.HandleFunc
+		// Optionally configure timeouts here, e.g.,
+		// ReadTimeout:  15 * time.Second,
+		// WriteTimeout: 15 * time.Second,
+		// IdleTimeout:  60 * time.Second,
 	}
+
+	// Canal para receber sinais de interrupção ou terminação
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	// Inicia o servidor em uma goroutine
+	go func() {
+		log.Println("Servidor iniciado na porta " + port + "...")
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Erro ao iniciar o servidor: %v\n", err)
+		}
+	}()
+
+	// Aguarda o sinal de interrupção
+	sig := <-sigChan
+	log.Printf("Sinal de desligamento recebido: %v, iniciando graceful shutdown...\n", sig)
+
+	// Cria um contexto com timeout para o shutdown
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 30*time.Second) // Ajuste o timeout conforme necessário
+	defer cancel()
+
+	// Inicia o processo de shutdown do servidor
+	if err := server.Shutdown(shutdownCtx); err != nil {
+		log.Fatalf("Erro durante o shutdown do servidor: %v\n", err)
+	}
+
+	log.Println("Servidor desligado com sucesso.")
 }
